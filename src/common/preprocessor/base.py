f20be9745c47c4bd103cf46b11f90034fa725fe4
ShaeerMoeed <shaeermoeed66@gmail.com>
2025-11-10 16:03:17 -0500

    Moved common preprocessor logic to base class

--- src/common/preprocessor/base.py
+++ src/common/preprocessor/base.py
@@ -1,3 +1,6 @@
+import os
+import uuid
+
 class Preprocessor:
 
     def __init__(self, parameter_set_list):
@@ -13,3 +16,67 @@ class Preprocessor:
     def build(self):
 
         pass
+
+
+    def create_run_folder(self, misc_args):
+
+        root_folder = misc_args['RootFolder']
+        run_id = misc_args['Uuid']
+        run_folder = os.path.join(root_folder, run_id)
+        os.mkdir(run_folder)
+
+        return run_folder
+
+
+    def get_run_id(self, misc_args):
+
+        if "Uuid" in misc_args:
+            run_id = misc_args["Uuid"]
+        else:
+            run_id = uuid.uuid4()
+
+        return run_id
+    
+
+    def check_input_provided(self, key, optional=False):
+
+        input_provided = key in self.parameter_set_list[0]
+        if not input_provided and not optional:
+            raise Exception("Missing required key: {}".format(key))
+        else:
+            return input_provided
+    
+
+    def check_unique_uuids(self):
+
+        input_provided = self.check_input_provided("Uuid", True)
+        if input_provided:
+            uuid_set = {self.parameter_set_list[i]["Uuid"] for i in range(self.num_parameter_sets)}
+            if len(uuid_set) != self.num_parameter_sets:
+                raise Exception("Specified uuids are not unique\n")
+
+
+    def check_single_input(self, key, optional):
+
+        input_provided = self.check_input_provided(key, optional)
+        if input_provided:
+            val = self.parameter_set_list[0][key]
+            for i in range(self.num_parameter_sets):
+                if self.parameter_set_list[i][key] != val:
+                    raise Exception("There should only be one input for key: {}".format(key))
+
+        return input_provided
+    
+
+    def extract_optional_input(self, key, unique=False):
+
+        if unique: 
+            if self.check_single_input(key, True):
+                return self.parameter_set_list[0][key]
+            else:
+                return None
+        else:
+            if self.check_input_provided(key, True):
+                return self.parameter_set_list[0][key]
+            else:
+                return None
--- src/simulators/qmc/long_range/preprocessor.py
+++ src/simulators/qmc/long_range/preprocessor.py
@@ -2,7 +2,6 @@ from src.common.preprocessor.base import Preprocessor
 from preprocess.system.base import System
 from preprocess.probability_table.factory import ProbabilityTableFactory
 from common.inputs.input_parser import InputParser
-import uuid
 import os
 
 class LongRangeQMC(Preprocessor):
@@ -11,7 +10,6 @@ class LongRangeQMC(Preprocessor):
 
         super.__init__(parameter_set_list)
 
-        self.input_format = None
         self.bin_folder = None
         self.cpp_source_folder = None
 
@@ -90,67 +88,3 @@ class LongRangeQMC(Preprocessor):
             raise Exception("No cpp binaries or source directory provided\n")
         else:
             self.driver_inputs = {"BinFolder": bin_folder, "SourceFolder": cpp_source_folder}
-            
-
-    def extract_optional_input(self, key, unique=False):
-
-        if unique: 
-            if self.check_single_input(key, True):
-                return self.parameter_set_list[0][key]
-            else:
-                return None
-        else:
-            if self.check_input_provided(key, True):
-                return self.parameter_set_list[0][key]
-            else:
-                return None
-            
-
-    def check_unique_uuids(self):
-
-        input_provided = self.check_input_provided("Uuid", True)
-        if input_provided:
-            uuid_set = {self.parameter_set_list[i]["Uuid"] for i in range(self.num_parameter_sets)}
-            if len(uuid_set) != self.num_parameter_sets:
-                raise Exception("Specified uuids are not unique\n")
-
-
-    def check_single_input(self, key, optional):
-
-        input_provided = self.check_input_provided(key, optional)
-        if input_provided:
-            val = self.parameter_set_list[0][key]
-            for i in range(self.num_parameter_sets):
-                if self.parameter_set_list[i][key] != val:
-                    raise Exception("There should only be one input for key: {}".format(key))
-
-        return input_provided
-    
-
-    def check_input_provided(self, key, optional=False):
-
-        input_provided = key in self.parameter_set_list[0]
-        if not input_provided and not optional:
-            raise Exception("Missing required key: {}".format(key))
-        else:
-            return input_provided
-
-
-    def get_run_id(self, misc_args):
-
-        if "Uuid" in misc_args:
-            run_id = misc_args["Uuid"]
-        else:
-            run_id = uuid.uuid4()
-
-        return run_id
-    
-
-    def create_run_folder(self, misc_args):
-
-        root_folder = misc_args['RootFolder']
-        run_id = misc_args['Uuid']
-        run_folder = os.path.join(root_folder, run_id)
-        os.mkdir(run_folder)
-
-        return run_folder

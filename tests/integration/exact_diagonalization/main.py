a9815550e354788c43e4107931595f2582ad7893
ShaeerMoeed <shaeermoeed66@gmail.com>
2026-02-06 10:55:49 -0500

    [Test] Add ED integration test

---
+++ tests/integration/exact_diagonalization/main.py
@@ -0,0 +1,6 @@
+from heisenberg_ion.interface.workflow import Workflow
+import os
+
+input_file = os.path.join(os.getcwd(),"tests/integration/exact_diagonalization/wrapper_inputs.txt")
+print(input_file)
+Workflow(input_file=input_file)

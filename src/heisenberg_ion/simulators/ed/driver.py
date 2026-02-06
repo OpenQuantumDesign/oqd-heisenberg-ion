d7a9af5a25a28adf07178f30e7d9beea0c2ec7d3
ShaeerMoeed <shaeermoeed66@gmail.com>
2026-02-06 10:51:53 -0500

    [Feat] ED driver so ED engine can be used with the wrapper via subprocess

--- src/heisenberg_ion/simulators/ed/driver.py
+++ src/heisenberg_ion/simulators/ed/driver.py
@@ -1,13 +1,27 @@
 from heisenberg_ion.common.driver.base import Driver
+import subprocess
+import os
 
 class ExactDiagonalization(Driver):
 
-    def __init__(self, root_folder):
+    def __init__(self, simulation_folder, simulator_inputs):
 
-        super().__init__(root_folder)
+        super().__init__(simulation_folder)
 
-        pass
+        self.input_file = os.path.join(simulation_folder, "inputs.txt")
+        self.julia_path = simulator_inputs["julia_path"]
+        self.ed_engine = os.path.dirname(os.path.abspath(__file__)) + "/engine.jl"
+
 
     def simulate(self):
 
-        pass
+        try:
+            subprocess.run([self.julia_path, self.ed_engine, self.input_file], check=True, 
+                           capture_output=True, text=True)
+        except subprocess.CalledProcessError as error:
+            print("Julia execution failed with exit code: {}\n".format(error.returncode))
+            print("STDOUT: {}".format(error.stdout))
+            print("STDERR:{}".format(error.stderr))
+            raise
+
+        return 0

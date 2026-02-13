2cc929d49e363180f0825a2defa5b6b2b0b9c4cc
ShaeerMoeed <shaeermoeed66@gmail.com>
2026-02-13 17:51:48 -0500

    [Refactor] Use J as the coefficient of only the XXZ term in ED

--- src/heisenberg_ion/simulators/ed/engine.jl
+++ src/heisenberg_ion/simulators/ed/engine.jl
@@ -26,7 +26,7 @@ function get_off_diag_term(Ket, i, j, r, theta)
     si = (Ket >> i) & 1
     sj = (Ket >> j) & 1
     mask = (si == sj) ? 0 : 1
-    return -0.5 * mask * exp((si-sj) * r * theta * 1im)
+    return -0.5 * mask * exp((si - sj) * r * theta * 1im)
 end
 
 
@@ -49,7 +49,7 @@ function hamiltonian_long_range(N, Delta, h, B, J_ij, J, Hamiltonian, theta)
     Dim = 2^N
 
     even_num_spins = (N % 2 == 0)
-    r_max = even_num_spins ? round(Int32, N/2)-1 : round(Int32, (N-1)/2)
+    r_max = even_num_spins ? round(Int32, N / 2) - 1 : round(Int32, (N - 1) / 2)
 
     for Ket = 0:Dim-1
         Diagonal = 0.0
@@ -59,25 +59,25 @@ function hamiltonian_long_range(N, Delta, h, B, J_ij, J, Hamiltonian, theta)
                 j = (i + r) % N
 
                 #S^Z part
-                Diagonal += Delta * J_ij[i+1,j+1] * get_zz_term(Ket, i, j)
+                Diagonal += J * Delta * J_ij[i+1, j+1] * get_zz_term(Ket, i, j)
 
                 #S^X part
                 Bra = get_non_zero_row_index(Ket, i, j)
-                Hamiltonian[Bra+1, Ket+1] += J_ij[i+1,j+1] * get_off_diag_term(Ket, i, j, r, theta)
+                Hamiltonian[Bra+1, Ket+1] += J * J_ij[i+1, j+1] * get_off_diag_term(Ket, i, j, r, theta)
             end
         end
-        
+
         if even_num_spins
-            for i=0:round(Int64, N/2)-1
-                r = round(Int64, N/2)
+            for i = 0:round(Int64, N / 2)-1
+                r = round(Int64, N / 2)
                 j = (i + r) % N
-                
+
                 #S^Z part
-                Diagonal += Delta * J_ij[i+1,j+1] * get_zz_term(Ket, i, j)
+                Diagonal += J * Delta * J_ij[i+1, j+1] * get_zz_term(Ket, i, j)
 
                 #S^X part
                 Bra = get_non_zero_row_index(Ket, i, j)
-                Hamiltonian[Bra+1, Ket+1] += J_ij[i+1,j+1] * get_off_diag_term(Ket, i, j, r, theta)
+                Hamiltonian[Bra+1, Ket+1] += J * J_ij[i+1, j+1] * get_off_diag_term(Ket, i, j, r, theta)
             end
         end
 
@@ -94,7 +94,7 @@ function hamiltonian_long_range(N, Delta, h, B, J_ij, J, Hamiltonian, theta)
         end
     end
 
-    return J * Hamiltonian
+    return Hamiltonian
 end
 
 function hamiltonian_nearest_neighbour(N, Delta, h, B, J, Hamiltonian, boundary, theta)
@@ -105,30 +105,30 @@ function hamiltonian_nearest_neighbour(N, Delta, h, B, J, Hamiltonian, boundary,
         Diagonal = 0.0
 
         for i = 0:N-2
-            j=i+1
+            j = i + 1
 
             #S^Z part
-            Diagonal += Delta * get_zz_term(Ket, i, j)
+            Diagonal += J * Delta * get_zz_term(Ket, i, j)
 
             Bra = get_non_zero_row_index(Ket, i, j)
             si = (Ket >> i) & 1
             sj = (Ket >> j) & 1
             mask = (si == sj) ? 0 : 1
-            Hamiltonian[Bra+1, Ket+1] += get_off_diag_term(Ket, i, j, 1, theta)
+            Hamiltonian[Bra+1, Ket+1] += J * get_off_diag_term(Ket, i, j, 1, theta)
         end
 
         if boundary == "periodic" && N != 2
-            i = N-1
+            i = N - 1
             j = 0
 
             #S^Z part
-            Diagonal += Delta * get_zz_term(Ket, i, j)
+            Diagonal += J * Delta * get_zz_term(Ket, i, j)
 
             Bra = get_non_zero_row_index(Ket, i, j)
             si = (Ket >> i) & 1
             sj = (Ket >> j) & 1
             mask = (si == sj) ? 0 : 1
-            Hamiltonian[Bra+1, Ket+1] += get_off_diag_term(Ket, i, j, 1, theta)
+            Hamiltonian[Bra+1, Ket+1] += J * get_off_diag_term(Ket, i, j, 1, theta)
         end
 
         #field along Z
@@ -145,7 +145,7 @@ function hamiltonian_nearest_neighbour(N, Delta, h, B, J, Hamiltonian, boundary,
 
     end
 
-    return J * Hamiltonian
+    return Hamiltonian
 
 end
 
@@ -178,8 +178,8 @@ function diagonalize_hamiltonian(; kwargs...)
         J_ij_file = kwargs[:J_ij_file]
         J_ij = readdlm(J_ij_file, ',', skipstart=1)
 
-        ed_parameters = (output_folder=kwargs[:run_folder], N=N, theta=theta, Delta=Delta, h=h, B=B, J=J, 
-        interaction_name=interaction_name,J_ij_file=J_ij_file)
+        ed_parameters = (output_folder=kwargs[:run_folder], N=N, theta=theta, Delta=Delta, h=h, B=B, J=J,
+            interaction_name=interaction_name, J_ij_file=J_ij_file)
 
         Hamiltonian = hamiltonian_long_range(N, Delta, h, B, J_ij, J, Hamiltonian, theta)
 
@@ -187,15 +187,15 @@ function diagonalize_hamiltonian(; kwargs...)
 
         boundary = kwargs[:boundary]
 
-        ed_parameters = (output_folder=kwargs[:run_folder], N=N, theta=theta, Delta=Delta, h=h, B=B, J=J, 
-        interaction_name=interaction_name,boundary=boundary)
+        ed_parameters = (output_folder=kwargs[:run_folder], N=N, theta=theta, Delta=Delta, h=h, B=B, J=J,
+            interaction_name=interaction_name, boundary=boundary)
 
         Hamiltonian = hamiltonian_nearest_neighbour(N, Delta, h, B, J, Hamiltonian, boundary, theta)
 
     else
         throw("interaction_type can be long_range or nearest_neighbor")
     end
-    
+
     Diag = eigen(Hamiltonian)
     evecs = Diag.vectors
     evals = Diag.values
@@ -222,7 +222,7 @@ function write_results(run_folder, evals, evecs)
 
     evals_df = DataFrame(evals=evals)
     CSV.write(ed_out_dir * "/energies.csv", evals_df)
-    
+
     column_names = [Symbol("|E_$i>") for i in 0:length(evals)-1]
     evecs_df = DataFrame(evecs, column_names)
     CSV.write(ed_out_dir * "/eigenvectors.csv", evecs_df)
@@ -232,7 +232,7 @@ end
 
 function extract_inputs(input_file_path)
 
-    input_parameters = Dict{Symbol, Vector}()
+    input_parameters = Dict{Symbol,Vector}()
 
     num_parameter_sets = 0
     simulation_folder = 0
@@ -259,8 +259,8 @@ function extract_inputs(input_file_path)
     input_parameters[Symbol("simulation_folder")] = fill(simulation_folder, num_parameter_sets)
 
     parameter_sets = []
-    for i=1:num_parameter_sets
-        single_parameter_set = Dict{Symbol, String}()
+    for i = 1:num_parameter_sets
+        single_parameter_set = Dict{Symbol,String}()
         for (key, val_list) in input_parameters
             single_parameter_set[key] = val_list[i]
         end
@@ -278,7 +278,7 @@ function execute_simulations(parameter_sets)
 
     for i = 1:num_parameter_sets
         kwargs = parameter_sets[i]
-        evals, evecs = diagonalize_hamiltonian(;kwargs...)
+        evals, evecs = diagonalize_hamiltonian(; kwargs...)
         write_results(kwargs[:run_folder], evals, evecs)
     end
 end

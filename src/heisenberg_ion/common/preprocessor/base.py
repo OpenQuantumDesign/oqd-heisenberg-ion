e857b5c480b11eca1271c5cb80423c827a5ffece
ShaeerMoeed <shaeermoeed66@gmail.com>
2026-02-16 14:35:49 -0500

    [Docs] Add docstrings to preprocessor base and factory

--- src/heisenberg_ion/common/preprocessor/base.py
+++ src/heisenberg_ion/common/preprocessor/base.py
@@ -4,9 +4,26 @@ from datetime import datetime
 
 
 class Preprocessor:
+    """
+    Preprocessor base class. Handles the preprocessing step before a driver is called
+    Carries a set of keys for which a single value should always be specified.
+    These are: "root_folder", "simulation_folder", "number_of_threads", "output_folder_name"
+
+    Raises:
+        Exception: if a required parameter is not specified
+        Exception: if a uuid is repeated
+        Exception: if multiple values are provided for a parameter that should have a single value
+    """
+
     keys_single_parameters = {"root_folder", "simulation_folder", "number_of_threads", "output_folder_name"}
 
     def __init__(self, parameter_set_list):
+        """
+        constructor for the Preprocessor base class
+
+        Args:
+            parameter_set_list (list[dict]): list of unprocessed parameter sets specified as dicts
+        """
 
         self.parameter_set_list = parameter_set_list
         self.num_parameter_sets = len(parameter_set_list)
@@ -17,10 +34,19 @@ class Preprocessor:
         self.driver_inputs = None
 
     def build(self):
+        """
+        Each subclass must implement a build method
+        """
 
         pass
 
     def create_output_folder(self):
+        """
+        creates the output folder to contain the simulation outputs inside the root folder
+
+        Returns:
+            (str): class attribute simulation_folder, the path to the simulation output folder
+        """
 
         output_folder_name_provided = self.check_single_input("output_folder_name", True)
 
@@ -36,6 +62,15 @@ class Preprocessor:
         return self.simulation_folder
 
     def create_run_folder(self, misc_args):
+        """
+        creates the run folder for a given parameter set inside the simulation output folder
+
+        Args:
+            misc_args (dict): contains the key "uuid" to specify the run_id corresponding to the associated parameter set
+
+        Returns:
+            (str): path to the parameter set run folder
+        """
 
         simulation_folder = self.simulation_folder
         run_id = str(misc_args["uuid"])
@@ -46,6 +81,15 @@ class Preprocessor:
         return run_folder
 
     def get_run_id(self, misc_args):
+        """
+        Extracts the uuid if specified, and creates one if not specified in the arguments
+
+        Args:
+            misc_args (dict): contains the key "uuid" to specify the run_id corresponding to the associated parameter set
+
+        Returns:
+            (str): the uuid associated with the parameter set
+        """
 
         if "uuid" in misc_args:
             run_id = misc_args["uuid"]
@@ -55,6 +99,19 @@ class Preprocessor:
         return run_id
 
     def check_input_provided(self, key, optional=False):
+        """
+        checks whether an input has been specified in the parameter set
+
+        Args:
+            key (str): _description_
+            optional (bool, optional): determines whether value associated with key is optional. Defaults to False.
+
+        Raises:
+            Exception: is a required input is not provided
+
+        Returns:
+            (bool): determines whether an input was provided for the given key
+        """
 
         input_provided = key in self.parameter_set_list[0]
         if not input_provided and not optional:
@@ -63,6 +120,12 @@ class Preprocessor:
             return input_provided
 
     def check_unique_uuids(self):
+        """
+        checks if multiple uuids are provided and if they are all unique. Does not throw an error if no uuid is provided
+
+        Raises:
+            Exception: if a uuid is repeated
+        """
 
         input_provided = self.check_input_provided("uuid", True)
         if input_provided:
@@ -71,6 +134,19 @@ class Preprocessor:
                 raise Exception("Specified uuids are not unique\n")
 
     def check_single_input(self, key, optional=False):
+        """
+        checks if multiple inputs are provided for a given key if it should have a single input
+
+        Args:
+            key (str): the parameter key
+            optional (bool, optional): specifies if the parameter is optional. Defaults to False.
+
+        Raises:
+            Exception: if multiple inputs provided for a key that should have a single input
+
+        Returns:
+            (bool): determines whether the specified input was provided
+        """
 
         input_provided = self.check_input_provided(key, optional)
         if input_provided:
@@ -82,6 +158,16 @@ class Preprocessor:
         return input_provided
 
     def extract_optional_input(self, key, unique=False):
+        """
+        extracts an optional input if provided and return None otherwise
+
+        Args:
+            key (str): parameter key
+            unique (bool, optional): determines if the value needs to be unique. Defaults to False.
+
+        Returns:
+            (str): the value associated with key if provided, otherwise returns None
+        """
 
         if unique:
             if self.check_single_input(key, True):
@@ -95,6 +181,9 @@ class Preprocessor:
                 return None
 
     def write_input_file(self):
+        """
+        writes a tab-delimited input file for the engine using the parameter set values
+        """
 
         simulation_folder = self.simulation_folder
         sse_input_file = os.path.join(simulation_folder, "inputs.txt")
@@ -107,5 +196,3 @@ class Preprocessor:
                         text_line += "," + str(self.parameter_set_list[i][key])
                 text_line += "\n"
                 f.write(text_line)
-
-        return 0
--- src/heisenberg_ion/common/preprocessor/factory.py
+++ src/heisenberg_ion/common/preprocessor/factory.py
@@ -4,16 +4,43 @@ from heisenberg_ion.simulators.qmc.nearest_neighbor.preprocessor import NearestN
 
 
 class PreprocessorFactory:
+    """
+    Factory for generating the required instance of the Preprocessor subclass. Carries a registry of Preprocessor subclasses
+
+    Raises:
+        Exception: if requested subclass is not found
+    """
+
     registry = {}
 
     def register(cls, name, subclass):
+        """
+        registers the specified subclass with the given name in the factory
+
+        Args:
+            name (str): name to be used while registering the specified subclass
+            subclass (Type[Preprocessor]): the subclass of preprocessor to be registered
+        """
 
         cls.registry[name] = subclass
 
     def create(cls, name, parameter_set_list):
+        """
+        creates an instance of the requested subclass
+
+        Args:
+            name (str): requested subclass name
+            parameter_set_list (list[dict]): list of parameter sets, each set specified as a dict, needed by the preprocessor constructor
+
+        Raises:
+            Exception: if requested Preprocessor subclass is not found
+
+        Returns:
+            (Preprocessor): instance of Preprocessor subclass requested
+        """
 
         if name not in cls.registry:
-            raise ValueError(f"Preprocessor implementation not found for name: {name}")
+            raise Exception(f"Preprocessor implementation not found for name: {name}")
         else:
             return cls.registry[name](parameter_set_list)
 

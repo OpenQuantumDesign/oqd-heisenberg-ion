961d287ee0fb0590ea00e11880f94d0e185a46ea
ShaeerMoeed <shaeermoeed66@gmail.com>
2026-02-16 14:35:03 -0500

    [Docs] Add docstrings to input utilities

--- src/heisenberg_ion/common/inputs/input_file_reader.py
+++ src/heisenberg_ion/common/inputs/input_file_reader.py
@@ -2,7 +2,17 @@ from .utils import convert_to_snake_case
 
 
 class InputFileReader:
+    """
+    Reads the tab-delimited input file and contains the parameters in a list of dicts. Each dict represents a parameter set
+    """
+
     def __init__(self, input_file_path, **kwargs):
+        """
+        InputFileReader constructor. Extracts parameter sets from file.
+
+        Args:
+            input_file_path (str): path to input file specifying parameter sets
+        """
 
         self.input_file = input_file_path
 
@@ -18,6 +28,12 @@ class InputFileReader:
         self.simulator = convert_to_snake_case(self.parameter_set_list[0]["simulator"])
 
     def extract_key_value_inputs(self):
+        """
+         Method for extracting key value pairs seperated by a tab from input file
+
+        Returns:
+            (dict): containing the key value pairs from the tab delimited input file
+        """
 
         input_config = {}
 
@@ -40,6 +56,17 @@ class InputFileReader:
         return input_config
 
     def record_input(self, input_config, key, data):
+        """
+        counts the parameter sets for a given key and records the key and the value in the input_config dictionary
+
+        Args:
+            input_config (dict): key value pairs container for input parameters
+            key (str): parameter key
+            data (str): parameter value
+
+        Returns:
+            (dict): containing the key value pairs from the tab delimited input file
+        """
 
         count_entries = len(data)
         self.count_parameter_sets(count_entries, key)
@@ -50,6 +77,16 @@ class InputFileReader:
         return input_config
 
     def override_file_inputs(self, input_config, **kwargs):
+        """
+        overrides file parameter specification with given parameter values
+
+        Args:
+            input_config (dict): key value pairs container for input parameters
+            **kwargs (dict): key word arguments containing the overriding keys and values
+
+        Returns:
+            (dict): key value pairs container for input parameters
+        """
 
         for key, val in kwargs.items():
             data = val.strip().split(",")
@@ -58,6 +95,16 @@ class InputFileReader:
         return input_config
 
     def count_parameter_sets(self, count_entries, key):
+        """
+        updates the number of parameter sets if more than one parameter set is specified for given key
+
+        Args:
+            count_entries (int): number of entries for given key
+            key (str): parameter key
+
+        Raises:
+            Exception: if number of values provided for key and number of parameter sets > 1 are inconsistent
+        """
 
         if count_entries != 1:
             if self.num_parameter_sets == 1:
@@ -66,11 +113,15 @@ class InputFileReader:
 
             # Can't have n>1 parameter sets in one field and m>1 parameter sets in another
             elif self.num_parameter_sets != count_entries:
-                raise ValueError(f"Inconistent number of entries for fields: {key} and {self.key_num_parameters_set}\n")
-
-        return 0
+                raise Exception(f"Inconistent number of entries for fields: {key} and {self.key_num_parameters_set}\n")
 
     def extract_parameter_set_list(self, input_config):
+        """
+        populates the list of parameter sets (class member) from the dictionary of input key value pairs
+
+        Args:
+            input_config (dict): contains the key value pairs specifying the parameter set
+        """
 
         self.parameter_set_list = [{} for i in range(self.num_parameter_sets)]
 
@@ -81,5 +132,3 @@ class InputFileReader:
             else:
                 for i in range(self.num_parameter_sets):
                     self.parameter_set_list[i][key] = val[i]
-
-        return 0

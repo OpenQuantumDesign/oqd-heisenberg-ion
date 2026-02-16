60ab0b9472cfe0885d6d3c201c5bb288446b9ec6
ShaeerMoeed <shaeermoeed66@gmail.com>
2026-02-16 14:35:28 -0500

    [Docs] Add docstrings to post processing utilities

--- src/heisenberg_ion/common/postprocess/ffs.py
+++ src/heisenberg_ion/common/postprocess/ffs.py
@@ -2,9 +2,18 @@ import numpy as np
 
 
 def free_fermion_energy(N, phi):
+    """
+    computes the ground state energy per site associated with a chain of free fermions
 
-    # N should not be divisible by 4
+    N should not be divisible by 4 for this calculation to yield the correct energy
 
+    Args:
+        N (int): number of sites
+        phi (float): boundary twist angle
+
+    Returns:
+        (float): energy per site
+    """
     s_min = -int(N / 2)
     E = 0.0
     for i in range(N):
@@ -13,14 +22,23 @@ def free_fermion_energy(N, phi):
         if eval < 0.0:
             E += eval
 
-    """
-    if (s != s_max):
-        raise Exception("s not equal to s_max")
-    """
+    # if (s != s_max):
+    #    raise Exception("s not equal to s_max")
+
     return E / N
 
 
 def free_fermion_stiffness(N, phi):
+    """
+    computes the spin stiffness associated with a chain of free fermions
+
+    Args:
+        N (int): number of sites
+        phi (float): boundary twist angle
+
+    Returns:
+        (float): spin stiffness
+    """
 
     E_phi = free_fermion_energy(N, phi)
     E_zero = free_fermion_energy(N, 0.0)
--- src/heisenberg_ion/common/postprocess/swt.py
+++ src/heisenberg_ion/common/postprocess/swt.py
@@ -2,6 +2,19 @@ import numpy as np
 
 
 def gamma_k(N, k, theta, alpha):
+    """
+    computes the coefficient gamma corresponding to the kth Fourier mode in linear spin wave theory for the power law decay spin-1/2 XY model
+    For details, see: https://arxiv.org/pdf/2601.20058
+
+    Args:
+        N (int): number of sites
+        k (int): Fourier mode
+        theta (float): twist angle
+        alpha (float): power law decay exponent in interaction couplings
+
+    Returns:
+        (float): gamma_k
+    """
 
     gamma = 0.0
     num_terms = int((N - 1) / 2)
@@ -12,6 +25,19 @@ def gamma_k(N, k, theta, alpha):
 
 
 def E_0_LSW(N, alpha, J, theta=0.0):
+    """
+    computes the ground state energy in linear spin wave theory for the power law decay spin-1/2 XY model.
+    For details, see: https://arxiv.org/pdf/2601.20058
+
+    Args:
+        N (int): number of sites
+        alpha (float): power law decay exponent
+        J (float): Energy scale
+        theta (float, optional): Boundary twist angle. Defaults to 0.0.
+
+    Returns:
+        (float): ground state energy in linear spin wave theory
+    """
 
     gamma_0 = gamma_k(N, 0, theta, alpha)
     energy = (-3.0 / 2.0) * N
@@ -26,6 +52,19 @@ def E_0_LSW(N, alpha, J, theta=0.0):
 
 
 def E_0_MF(N, alpha, J, theta=0.0):
+    """
+    computes the ground state energy in mean field theory for the power law decay spin-1/2 XY model.
+    For details, see: https://arxiv.org/pdf/2601.20058
+
+    Args:
+        N (int): number of sites
+        alpha (float): power law decay exponent
+        J (float): Energy scale
+        theta (float, optional): Boundary twist angle. Defaults to 0.0.
+
+    Returns:
+        (float): ground state energy in mean field theory
+    """
 
     gamma_0 = gamma_k(N, 0, theta, alpha)
 
@@ -35,6 +74,17 @@ def E_0_MF(N, alpha, J, theta=0.0):
 
 
 def E_0_LSW_NN(N, J):
+    """
+    computes the ground state energy in linear spin wave theory for the nearest neighbor spin-1/2 XY model.
+    For details, see: https://arxiv.org/pdf/2601.20058
+
+    Args:
+        N (int): number of sites
+        J (float): Energy scale
+
+    Returns:
+        (float): ground state energy in linear spin wave theory
+    """
 
     energy = (-3.0 / 2.0) * N
 
@@ -55,6 +105,20 @@ def rho(N, alpha, J):
 
 
 def rho_2(N, alpha, theta, J_1, J_2=1.0):
+    """
+    computes the spin stiffness for the power law decay spin-1/2 XY model in linear spin wave theory
+    For details, see: https://arxiv.org/pdf/2601.20058
+
+    Args:
+        N (int): number of sites
+        alpha (float): power law decay exponent
+        theta (float): twist angle
+        J_1 (float): energy scale
+        J_2 (float, optional): energy scale. Defaults to 1.0. Should typically be set equal to J_1
+
+    Returns:
+        (float): spin wave theory spin stiffness
+    """
 
     energy_theta = E_0_LSW(N, alpha, J_1, theta)
     energy_0 = E_0_LSW(N, alpha, J_2, 0.0)
@@ -64,6 +128,19 @@ def rho_2(N, alpha, theta, J_1, J_2=1.0):
 
 
 def rho_mf(N, alpha, theta, J):
+    """
+    computes the spin stiffness for the power law decay spin-1/2 XY model in mean field theory
+    For details, see: https://arxiv.org/pdf/2601.20058
+
+    Args:
+        N (int): number of sites
+        alpha (float): power law decay exponent
+        theta (float): twist angle
+        J (float): energy scale
+
+    Returns:
+        (float): mean field theory spin stiffness
+    """
 
     energy_theta = E_0_MF(N, alpha, J, theta)
     energy_0 = E_0_MF(N, alpha, J, 0.0)

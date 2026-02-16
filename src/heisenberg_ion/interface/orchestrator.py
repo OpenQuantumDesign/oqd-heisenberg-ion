3b9bb1b6f0f822dbc0fd1629aea122b1b149f501
ShaeerMoeed <shaeermoeed66@gmail.com>
2026-02-16 14:36:06 -0500

    [Docs] Add docstrings to interface

--- src/heisenberg_ion/interface/orchestrator.py
+++ src/heisenberg_ion/interface/orchestrator.py
@@ -4,7 +4,17 @@ from ..common.preprocessor.factory import PreprocessorFactory
 
 
 class Orchestrator:
+    """
+    Responsible for managing the entire workflow of the package. Sequentially reads the inputs, calls the preprocessor, driver and simulator
+    """
+
     def __init__(self, **kwargs):
+        """
+        constructor for the Orchestrator class. Determines whether inputs should be read from file or are provided as key word arguments
+
+        Args:
+            **kwargs (dict): key word arguments. An input file is expected if one of the keys is 'input_file'
+        """
 
         if "input_file" in kwargs:
             self.build_from_file(**kwargs)
@@ -12,6 +22,15 @@ class Orchestrator:
             self.build_from_parameters(**kwargs)
 
     def build_from_file(self, input_file, **kwargs):
+        """
+        used if input file needs to be read for the simulation.
+
+        Args:
+            input_file (str): path to the input file
+
+        Returns:
+            (int): 0 if the program executes to completion
+        """
 
         file_inputs = InputFileReader(input_file, **kwargs)
         simulator = file_inputs.simulator
@@ -24,6 +43,12 @@ class Orchestrator:
         return 0
 
     def build_from_parameters(self, **kwargs):
+        """
+        used if the simulation inputs are provided as key word arguments
+
+        Returns:
+            (int): 0 if the program executes to completion
+        """
 
         simulator = kwargs["simulator"]
         parameter_set_list = [kwargs]

90a9a78762eb074559f26e1a503fa795d26654df
ShaeerMoeed <shaeermoeed66@gmail.com>
2026-02-16 14:36:59 -0500

    [Docs] Add docstring to ED preprocessor and driver

--- src/heisenberg_ion/simulators/ed/preprocessor.py
+++ src/heisenberg_ion/simulators/ed/preprocessor.py
@@ -8,7 +8,17 @@ from ..preprocess.system.base import System
 
 
 class ExactDiagonalization(Preprocessor):
+    """
+    Preprocessor subclass for Exact Diagonalization. Validates inputs, configures the parameter sets and writes the engine input file
+    """
+
     def __init__(self, parameter_set_list):
+        """
+        constructor for the ED Preprocessor.
+
+        Args:
+            parameter_set_list (list[dict]): list of parameter sets specified with parameter set defined by a dict
+        """
 
         super().__init__(parameter_set_list)
 
@@ -17,6 +27,10 @@ class ExactDiagonalization(Preprocessor):
         self.build()
 
     def build(self):
+        """
+        Preprocesses the simulation parameter sets, and validates root output folder and uuids.
+        Also populates the ED Driver inputs.
+        """
 
         self.check_single_input("root_folder")
         self.root_folder = self.parameter_set_list[0]["root_folder"]
@@ -30,6 +44,9 @@ class ExactDiagonalization(Preprocessor):
         self.configure_simulation()
 
     def configure_simulation(self):
+        """
+        Configures all the parameter sets sequentially. Then writes the input file for the engine.
+        """
 
         for i in range(self.num_parameter_sets):
             parameter_set = self.parameter_set_list[i]
@@ -39,6 +56,14 @@ class ExactDiagonalization(Preprocessor):
         self.write_input_file()
 
     def configure_parameter_set(self, parameter_args):
+        """
+        Configures a single parameter set.
+        Determines the run_id, creates the parameter set output folder and parses the str inputs.
+        Also defines the system and appends any required parameters to the parameter set.
+
+        Args:
+            parameter_args (dict): Parameter set arguments specified as key value pairs. Single value for each key specified as a str
+        """
 
         input_config = InputParser(**parameter_args)
         system_args = input_config.simulation_config["system"]
@@ -65,23 +90,23 @@ class ExactDiagonalization(Preprocessor):
             misc_args["J_ij_file"] = J_ij_file_path
             parameter_args["J_ij_file"] = J_ij_file_path
 
-        """
-        theta = system_args['theta']
+        # theta = system_args['theta']
 
-        ed_parameters = EDParameters(system, run_folder, theta)
+        # ed_parameters = EDParameters(system, run_folder, theta)
 
-        self.driver_inputs.append(ed_parameters)
-        """
-
-        return 0
+        # self.driver_inputs.append(ed_parameters)
 
     def extract_cli_requirements(self):
+        """
+        Extracts the Julia path if specified as a user input. If not provided, attempts to find the Julia path and throws an error if unsucessful.
+        If the path is available, populates the ED Driver inputs
+        """
 
         julia_path = self.extract_optional_input("julia_path", True)
         if julia_path is None:
             julia_path = sh.which("julia")
             if julia_path is None:
-                raise Exception("No cpp binaries or source directory provided\n")
+                raise Exception("No Julia path provided\n")
 
         self.driver_inputs = {"julia_path": julia_path}
 

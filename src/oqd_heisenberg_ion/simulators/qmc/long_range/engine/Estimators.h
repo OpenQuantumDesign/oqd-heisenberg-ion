d54f145d605749c7b886332f87cbe6844a186cb4
ShaeerMoeed <shaeermoeed66@gmail.com>
2026-04-24 22:00:33 -0400

    [Feature] Add functionality to write shot data to .h5 files instead of .csv files

--- src/oqd_heisenberg_ion/simulators/qmc/long_range/engine/Estimators.h
+++ src/oqd_heisenberg_ion/simulators/qmc/long_range/engine/Estimators.h
@@ -1,5 +1,6 @@
 #ifndef cpp_qmc_ESTIMATORS_H
 #define cpp_qmc_ESTIMATORS_H
+#include <hdf5.h>
 #include<vector>
 #include<filesystem>
 #include "SimulationParameters.h"
@@ -16,14 +17,24 @@ private:
 
     int simulation_steps;
 
-    std::vector<std::vector<int>> step_spin_configs;
+    std::vector<int8_t> step_spin_configs;
+    //std::vector<std::vector<int>> step_spin_configs_diagnostics;
+    int step_spin_config_offset = 0;
     bool track_spin_configs;
 
     std::vector<double> step_spin_stiffness;
 
     std::vector<double> step_S_k_pi;
 
+    hid_t shot_data_file_id, shot_data_dataset_id, shot_data_dataspace_id, dataset_create_property_list_id;
+    int num_updates = 0;
+
+    void initializeShotStorage(const SimulationParameters &sim_params);
+
 public:
+
+    uint64_t chunk_size;
+
     Estimators(const SimulationParameters &sim_params, const bool &track_spin_configs,
         std::shared_ptr<spdlog::logger> &logger_ptr);
 
@@ -36,15 +47,17 @@ public:
                                           const SimulationParameters &sim_params, const double &spectrum_offset,
                                           const double &winding, const std::vector<std::vector<int>> &lattice_sites);
 
-    void trackSpinConfigs(const std::vector<int> &spin_configs) {
-        step_spin_configs.push_back(spin_configs);
-    }
+    void trackSpinConfigs(const std::vector<int> &spin_configs, const SimulationParameters &sim_params);
 
-    void outputStepData(const SimulationParameters &sim_params) const;
+    void outputStepData() const;
 
-    void outputDiagnostics(const SimulationParameters &sim_params) const;
+    //void outputDiagnostics(const SimulationParameters &sim_params) const;
 
-    void outputPairCorrelations(const SimulationParameters &sim_params) const;
+    void populateShotDataFile(const SimulationParameters &sim_params);
+
+    //void outputPairCorrelations(const SimulationParameters &sim_params) const;
+
+    void closeShotDataContainers() const;
 
     //void outputClusterHistogram(const SimulationParameters &sim_params, const std::vector<int> &cluster_probs) const;
 
